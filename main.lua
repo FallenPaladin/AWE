9843dd98695778673fa253bede600b179224544b
Micheal Earl <michealearldev@gmail.com>
2018-04-29 18:12:35 -0700

    table example

--- main.lua
+++ main.lua
@@ -1,57 +1,57 @@
 function love.load()
-  positionX = 400
-  positionY = 300
-  width = 30
-  height = 30
-  speed = 100
+  -- this is a table containing data
+  player = { x = 400, y = 300, width = 30, height = 30, spd = 200 }
 
-  enemyPositionX = 200
-  enemyPositionY = 500
-  enemyWidth = 30
-  enemyHeight = 30
-  enemySpeed = 100
+  -- this is a table containing tables which contain data
+  enemies = {
+    enemy1 = { x = 500, y = 300, width = 30, height = 30, spd = 100 },
+    enemy2 = { x = 300, y = 200, width = 30, height = 30, spd = 100 }
+  }
 end
 
 function love.update(dt)
   if love.keyboard.isDown( 'w' ) then
-    positionY = positionY - speed*dt
+    player.y = player.y - player.spd*dt
   end
   if love.keyboard.isDown( 'a' ) then
-    positionX = positionX - speed*dt
+    player.x = player.x - player.spd*dt
   end
   if love.keyboard.isDown( 's' ) then
-    positionY = positionY + speed*dt
+    player.y = player.y + player.spd*dt
   end
   if love.keyboard.isDown( 'd' ) then
-    positionX = positionX + speed*dt
+    player.x = player.x + player.spd*dt
   end
 
-  if positionY < enemyPositionY then
-    enemyPositionY = enemyPositionY - 1
+  -- using a for loop to iterate through the table of enemies and perform all of their logic
+  for _, enemy in pairs(enemies) do
+    if player.y < enemy.y then
+      enemy.y = enemy.y - enemy.spd*dt
+    end
+    if player.y > enemy.y then
+      enemy.y = enemy.y + enemy.spd*dt
+    end
+    if player.x < enemy.x then
+      enemy.x = enemy.x - enemy.spd*dt
+    end
+    if player.x > enemy.x then
+      enemy.x = enemy.x + enemy.spd*dt
+    end 
   end
-  if positionY > enemyPositionY then
-    enemyPositionY = enemyPositionY + 1
-  end
-  if positionX < enemyPositionX then
-    enemyPositionX = enemyPositionX -1
-  end
-  if positionX > enemyPositionX then
-    enemyPositionX = enemyPositionX + 1
-  end 
-  --if love.keyboard.isDown( 'q' ) then
-    
-  --if love.keyboard.isDown( 'e' ) then
-  
-  --if love.keyboard.isDown( 'r' ) then  
 end
 
 function love.draw()
-  --Player Character Color
+  -- player color
   love.graphics.setColor(1, 1, 1)
-  --love.graphics.rectangle( mode, x, y, width, height )
-  love.graphics.rectangle( 'fill', positionX, positionY, width, height )
+
+  -- draw the player
+  love.graphics.rectangle( 'fill', player.x, player.y, player.width, player.height )
+  
+  -- enemy color
   love.graphics.setColor(1, 0, 0)
-  love.graphics.rectangle( 'fill', enemyPositionX, enemyPositionY, enemyWidth, enemyHeight)
-  
-  
+
+  -- using a for loop to iterate through the table of enemies and draw all of them
+  for _, enemy in pairs(enemies) do
+    love.graphics.rectangle( 'fill', enemy.x, enemy.y, enemy.width, enemy.height)
+  end
 end
